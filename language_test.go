f3b0244d67603e8f7908dc481cff7d5039d73244
Elviss Strazdins <elviss@elviss.lv>
2025-02-17 12:07:03 +0200

    Make language tags fixed case

--- language_test.go
+++ language_test.go
@@ -13,14 +13,15 @@ func TestNewLanguage(t *testing.T) {
 		result contenttype.Language
 	}{
 		{name: "Empty string", value: "", result: contenttype.Language{}},
-		{name: "Language and region", value: "lv-LV", result: contenttype.Language{Language: "lv", Script: "", Region: "lv"}},
-		{name: "Language, script, and region", value: "en-latn-US", result: contenttype.Language{Language: "en", Script: "latn", Region: "us"}},
+		{name: "Language and region", value: "lv-LV", result: contenttype.Language{Language: "lv", Script: "", Region: "LV"}},
+		{name: "Language, script, and region", value: "lv-Latn-LV", result: contenttype.Language{Language: "lv", Script: "Latn", Region: "LV"}},
+		{name: "Language, lower-case script, and region", value: "en-latn-US", result: contenttype.Language{Language: "en", Script: "Latn", Region: "US"}},
 		{name: "Language only", value: "lt", result: contenttype.Language{Language: "lt", Script: "", Region: ""}},
-		{name: "Language and lowercase region", value: "lv-lv", result: contenttype.Language{Language: "lv", Script: "", Region: "lv"}},
+		{name: "Language and lowercase region", value: "lv-lv", result: contenttype.Language{Language: "lv", Script: "", Region: "LV"}},
 		{name: "Language and region number", value: "lv-428", result: contenttype.Language{Language: "lv", Script: "", Region: "428"}},
 		{name: "Three letter language", value: "lav", result: contenttype.Language{Language: "lav", Script: "", Region: ""}},
 		{name: "Language and character variant", value: "sl-rozaj", result: contenttype.Language{Language: "sl", Script: "", Region: "", Variant: "rozaj"}},
-		{name: "Language, region, and digit variant", value: "de-CH-1901", result: contenttype.Language{Language: "de", Script: "", Region: "ch", Variant: "1901"}},
+		{name: "Language, region, and digit variant", value: "de-CH-1901", result: contenttype.Language{Language: "de", Script: "", Region: "CH", Variant: "1901"}},
 	}
 
 	for _, testCase := range testCases {
